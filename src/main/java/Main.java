9235ed3f0c46185348df5eb7f16a2cc1162ffb2c
Viktor <v.kurochkin89@yandex.ru>
2024-02-18 09:45:38 +0300

    Описал Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+
+        HotDrink coffee = new HotDrink("Coffee", 2.5, 90);
+        HotDrink tea = new HotDrink("Tea", 2.0, 85);
+
+        System.out.println(coffee);
+        System.out.println(tea);
+
+    }
 }
