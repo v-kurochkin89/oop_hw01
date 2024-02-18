dea0aad4f9213b71612d0b11f04ca524cdc0ff87
Viktor <v.kurochkin89@yandex.ru>
2024-02-18 09:44:45 +0300

    Добавил класс HotDrink

--- src/main/java/HotDrink.java
+++ src/main/java/HotDrink.java
@@ -1,2 +1,25 @@
-public class HotDrink {
+public class HotDrink extends Product {
+    private int temperature;
+
+    public HotDrink(String name, double price, int temperature) {
+        super(name, price);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrink{" +
+                "name=" + getName() +
+                ", price=" + getPrice() +
+                ", temperature=" + temperature + "°C" +
+                '}';
+    }
 }
